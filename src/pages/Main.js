dc8ce8e0d56f2409d4dc293b112ea08587020e7f
suan1212 <ksja3014@gmail.com>
2021-01-13 12:04:07 +0900

    프론트작업

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
 import { Helmet } from 'react-helmet';
+import array from './1.png'; 
+import add from './2.PNG';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
+ 
 import './Main.css';
 
+ 
 
 class Main extends Component {
     constructor(props){
@@ -21,8 +27,8 @@ class Main extends Component {
                     </title>
                 </Helmet>
                 <h2 className="text">환영합니다</h2>
-                <h1 className="text">TEAM-ARRAY[]는 생각보다...</h1>
-            </div>
+                <h1 className="text">TEAM-ARRAY[]는 생각보다...</h1><img src={array} alt="array" />;  <img src={add} alt="add" />;
+                </div>
         );
     }
 }
